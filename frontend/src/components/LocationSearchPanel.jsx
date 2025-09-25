dc6be1f5996d2ac8f2a1be2835d4cd32e7778bbf
vivek-sahane <viveksahane1528@gmail.com>
2025-09-25 16:54:05 +0530

    frontend pages Home cars routesSearch

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 
-const LocationSearchPanel = () => {
+const LocationSearchPanel = ({ setPanelOpen, setVehiclePanel }) => {
+  // sample array for locations
+  const locations = [
+    "24B, Near Kappor's cafe, Shree ram mandir, Bhopal",
+    "22B, Near Kappor's cafe, Shree ram mandir, Bhopal",
+    "12B, Near Kappor's cafe, Shree ram mandir, Bhopal",
+    "10B, Near Kappor's cafe, Shree ram mandir, Bhopal",
+  ]
+
   return (
-    <div>LocationSearchPanel</div>
+    <div className='flex-col top-10'>
+      {/* this is just a sample data */}
+      {
+        locations.map((elem, idx) => (
+          <div 
+            key={idx}
+            onClick={() => {
+              setVehiclePanel(true)   // show vehicles panel
+              setPanelOpen(false)     // close location search
+            }}
+            className='flex gap-4 border-2 p-3 border-gray-50 active:border-black rounded-xl items-center my-2 justify-start '
+          >
+            <h2 className='bg-[#eee] h-8 flex items-center justify-center w-12 rounded-full'>
+              <i className='ri-map-pin-fill text-xl'></i>
+            </h2>
+            <h4 className='font-medium'>{elem}</h4>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,6 +12,8 @@ const Home = () => {
   const [panelOpen, setPanelOpen] = useState(false)
   const panelRef = useRef(null)
   const panelCloseRef = useRef(null)
+  const [vehiclePanel, setVehiclePanel] = useState(false)
+  const vehiclePanelRef = useRef(null)
 
   const submitHandler = (e) => {
     e.preventDefault()
@@ -40,14 +42,31 @@ const Home = () => {
     }
   },[panelOpen])
 
+  useGSAP(function() {
+    if(vehiclePanel) {
+      gsap.to(vehiclePanelRef.current, {
+        transform:'translateY(0)'
+      })
+    }
+    else{
+      gsap.to(vehiclePanelRef.current, {
+        transform:'translateY(100%)'
+      })
+    }
+  },[vehiclePanel])
+
   return (
     <div className='h-screen relative'>
 
       <img src={logo} alt="MargDarshak Logo" className="w-16 absolute left-5 top-5 mb-10 " />
 
-      <div className='h-screen w-screen'>
-        {/* image for temporary use  */}
-        <img className='h-full w-full object-cover' src='https://www.medianama.com/wp-content/uploads/2018/06/Screenshot_20180619-112715.png.png' alt='' />
+      <div 
+        onClick={()=> {
+          setVehiclePanel(false)
+        }}
+        className='h-screen w-screen'>
+          {/* image for temporary use  */}
+          <img className='h-full w-full object-cover' src='https://www.medianama.com/wp-content/uploads/2018/06/Screenshot_20180619-112715.png.png' alt='' />
       </div>
 
       <div className='bg-white flex flex-col justify-end h-screen absolute top-0 w-full'>
@@ -98,8 +117,47 @@ const Home = () => {
           </form>
         </div>
         <div ref={panelRef} className=' bg-white h-0'>
-            <LocationSearchPanel/>
+            <LocationSearchPanel  setPanelOpen={setPanelOpen} setVehiclePanel={setVehiclePanel} />
         </div>
+      </div>
+
+      <div ref={vehiclePanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-3 py-8'>
+        <h3 className='text-2xl font-semibold mb-5'>Choose a Vehicle</h3>
+
+        {/** Div:1 for location */}
+        <div className='flex border-2 active:border-black mb-2 rounded-xl p-3 w-full items-center justify-between '>
+          <img className='h-12'  src='https://tse2.mm.bing.net/th/id/OIP.jD4muZBvbCYzo-cmaSVl3QHaFj?pid=Api&P=0&h=180' />
+          
+          <div className='ml-2 w-1/2'>
+            <h4 className='font-medium text-base'>UberGo <span><i className="ri-user-3-fill"></i>4</span></h4>
+            <h5 className='font-medium text-sm'>2 mins away</h5>
+            <p className='font-normal text-xs text-gray-600'>Affordable, compact rides</p>
+          </div>
+          <h2 className='text-xl font-semibold'>193.20 $</h2>
+        </div>
+
+        <div className='flex border-2 active:border-black mb-2 rounded-xl p-3 w-full items-center justify-between '>
+          <img className='h-12'  src='https://tse2.mm.bing.net/th/id/OIP.jD4muZBvbCYzo-cmaSVl3QHaFj?pid=Api&P=0&h=180' />
+          
+          <div className='ml-2 w-1/2'>
+            <h4 className='font-medium text-base'>UberGo <span><i className="ri-user-3-fill"></i>4</span></h4>
+            <h5 className='font-medium text-sm'>2 mins away</h5>
+            <p className='font-normal text-xs text-gray-600'>Affordable, compact rides</p>
+          </div>
+          <h2 className='text-xl font-semibold'>193.20 $</h2>
+        </div>
+
+        <div className='flex border-2 active:border-black mb-2 rounded-xl p-3 w-full items-center justify-between '>
+          <img className='h-12'  src='https://tse3.mm.bing.net/th/id/OIP.sUxREtT1w4CJlmY03I0ZBgHaEk?pid=Api&P=0&h=180https://tse3.mm.bing.net/th/id/OIP.sUxREtT1w4CJlmY03I0ZBgHaEk?pid=Api&P=0&h=180' />
+          
+          <div className='ml-2 w-1/2'>
+            <h4 className='font-medium text-base'>UberGo <span><i className="ri-user-3-fill"></i>4</span></h4>
+            <h5 className='font-medium text-sm'>2 mins away</h5>
+            <p className='font-normal text-xs text-gray-600'>Affordable, compact rides</p>
+          </div>
+          <h2 className='text-xl font-semibold'>193.20 $</h2>
+        </div>
+
       </div>
 
     </div>
